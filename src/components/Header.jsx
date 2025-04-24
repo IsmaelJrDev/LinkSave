7b871f992ee526e4b2efda1a0b80b52ddd199fbd
IsmaelJrDev <ismaelbibiano.dev@gmail.com>
2025-04-24 02:18:06 -0600

    Update project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="bg-dark text-light p-3 text-left ">
+            <i class="bi bi-cloud-download-fill"></i>LinkSave
+        </header>
+    );
+}
+
+export default Header;
