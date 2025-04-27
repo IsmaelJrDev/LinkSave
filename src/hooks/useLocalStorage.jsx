61ed901c1c5bfa406ece436929b4d955cbe22db9
IsmaelJrDev <ismaelbibiano.dev@gmail.com>
2025-04-26 19:58:55 -0600

    Actualizacion de app de prueba

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,26 +1,26 @@
 import { useEffect } from "react";
 
 function useLocalStorage(key, valorInicial) {
-  const [valor, setValor] = useState(() => {
-    // funcion para guardar el valor
-    const datosGuardados = localStorage.getItem(key); // obtener datos por llave
-    return datosGuardados // validacion de datos
-      ? JSON.parse(datosGuardados) // convertir a objeto de js
-      : valorInicial; // si no hay datos, se guarda el valor inicial
-  });
+    const [valor, setValor] = useState(() => {
+        // funcion para guardar el valor
+        const datosGuardados = localStorage.getItem(key); // obtener datos por llave
+        return datosGuardados // validacion de datos
+            ? JSON.parse(datosGuardados) // convertir a objeto de js
+            : valorInicial; // si no hay datos, se guarda el valor inicial
+    });
 
-  useEffect(() => {
-    //funcion que se ejcuatara cada vez que cambie el valor
-    localStorage.setItem(
-      key, // selecciona la llave
-      JSON.stringify(valor) // convertir de js u objeto a JSON
-    );
-  }, [key, valor]); // se ejecuta cada vez que cambia el valor y se inicializa el key
+    useEffect(() => {
+        //funcion que se ejcuatara cada vez que cambie el valor
+        localStorage.setItem(
+            key, // selecciona la llave
+            JSON.stringify(valor) // convertir de js u objeto a JSON
+        );
+    }, [key, valor]); // se ejecuta cada vez que cambia el valor y se inicializa el key
 
-  // SE USA EN EL CONTEXT
-  // const [urls, setUrls] = useLocalStorage("urls", []); // se inicializa el valor de urls como un array vacio]
+    // SE USA EN EL CONTEXT 
+    // const [urls, setUrls] = useLocalStorage("urls", []); // se inicializa el valor de urls como un array vacio]
 
-  return [valor, setValor];
+    return [valor, setValor];
 }
 
 export default localStorage;
